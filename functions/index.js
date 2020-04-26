3e4d941f0c7dbc84316c3293c9d88852a36f4769
kzielinski96 <kornel.zielinski96@gmail.com>
2020-04-26 19:04:37 +0200

    Added basic Cloud Speech functionality

--- functions/index.js
+++ functions/index.js
@@ -6,11 +6,46 @@ admin.initializeApp();
 express = require("express");
 const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-
 app.get("/helloWorld", (req, res) => {
   res.send("Hello from Speech2Text project!");
 });
 
 exports.api = functions.https.onRequest(app);
+
+exports.createAudioLabels = functions.firestore
+  .document("samples/{sampleid}")
+  .onCreate(async (snap, context) => {
+    const newDoc = snap.data();
+    const file_path = newDoc.sample_path;
+    const doc_id = snap.id;
+    const speech = require("@google-cloud/speech");
+    const client = new speech.SpeechClient();
+
+    var setWithOptions;
+
+    const audio = {
+      uri: file_path,
+    };
+    const config = {
+      encoding: "LINEAR16",
+      sampleRateHertz: 24000,
+      languageCode: "en-US",
+    };
+    const request = {
+      audio: audio,
+      config: config,
+    };
+
+    const [response] = await client.recognize(request);
+    const transcritpion = response.results
+      .map((result) => result.alternatives[0].transcript)
+      .join("\n");
+
+    var db = admin.firestore();
+    var docRef = db.collection("samples").doc(doc_id);
+    setWithOptions = docRef.update({
+      sample_content: transcritpion,
+    });
+
+    return Promise.all([setWithOptions]);
+  });
