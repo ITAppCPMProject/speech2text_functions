23185e3b0fd9435014fa1f24059f78eca39b7839
kurniak <kkurnik@protonmail.com>
2020-05-03 17:52:47 +0200

    Add signup basic functionality

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,22 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const app = require("express")();
 
 admin.initializeApp();
 
-express = require("express");
-const app = express();
+const config = {
+  apiKey: "AIzaSyA9QN2KLd2Dh9HaVGCvV5URt08hLSxssBQ",
+  authDomain: "it-app-cpm.firebaseapp.com",
+  databaseURL: "https://it-app-cpm.firebaseio.com",
+  projectId: "it-app-cpm",
+  storageBucket: "it-app-cpm.appspot.com",
+  messagingSenderId: "618722655546",
+  appId: "1:618722655546:web:e0e77028c7482a5fc36403",
+  measurementId: "G-ZCR718NH7R"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
 
 app.get("/helloWorld", (req, res) => {
   res.send("Hello from Speech2Text project!");
@@ -49,3 +61,23 @@ exports.createAudioLabels = functions.firestore
 
     return Promise.all([setWithOptions]);
   });
+
+// Signup
+  app.post('/signup', (req, res) => {
+    const newUser = {
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+      handle: req.body.handle,
+    }
+
+// TODO: validate data
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+      .then( data => {
+        return res.status(201).json({ message: `user ${data.user.uid} signed up successfully`});
+      })
+      .catch( err => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      })
+  })
